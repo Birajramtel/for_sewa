74e9e81adeb73b4c8229e179b5b7a9e32967abee
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-09 17:20:48 +0545

    Changes made in office

--- index.php
+++ index.php
@@ -102,22 +102,33 @@
     <section>
       <!-- Forsewa Key Activities Section Opened -->
       <div class="container">
-        <div class="row d-flex justify-content-center">
-          <div class="col-4">
-            <img src="img/activities_1.png" alt="">
+        <div class="card-group">
+          <div class="card">
+            <img src="img/activities_1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
           </div>
-          <div class="col-4">
-            <img src="img/activities_1.png" alt="">
+          <div class="card">
+            <img src="img/activities_1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
           </div>
-          <div class="col-4">
-            <img src="img/activities_1.png" alt="">
-          </div>
-          <div class="col-4">
-            <img src="img/activities_1.png" alt="">
+          <div class="card">
+            <img src="img/activities_1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
           </div>
         </div>
       </div>
-      
       <!-- Forsewa Key Activities Section Closed -->
 
       <!-- Package Image Opened -->
