be5df1d9b7e89cc5893846947cdf345ee6e4cd5a
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-13 16:58:36 +0545

    Changes made in Home page

--- index.php
+++ index.php
@@ -89,42 +89,68 @@
 
     <!-- Section Opened -->
     <section>
-        <!-- Forsewa Key Activities Section Opened -->
-        <div class="container mt-5">
-            <h1 class="mb-5">For Activities</h1>
+         <!-- Forsewa Key Activities Section Opened -->
+         <div class="container mt-5">
+            <h1 class="mb-5">ForSewa Key Activities</h1>
             <div class="card-group activity_image">
-                <div class="card">
-                    <img src="img/activities_1.png" class="card-img-top " alt="Import and Export">
+                <div class="card text-center">
+                    <img src="img/activities_1.png" class="card-img" alt="Import and Export">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body p-5 m-5 text-white">Import and Export</h3>
+                    </div>
                 </div>
-                <h2 class="activity_title">Import and Export</h2>
-                <div class="card">
-                    <img src="img/activities_2.png" class="card-img-top" alt="...">
+                <div class="card text-center">
+                    <img src="img/activities_2.png" class="card-img" alt="Hardware">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body p-5 m-5 text-white">Hardware</h3>
+                    </div>
                 </div>
-                <div class="card">
-                    <img src="img/activities_3.png" class="card-img-top" alt="...">
-                </div>
+                <div class="card text-center">
+                    <img src="img/activities_3.png" class="card-img" alt="Real Estate">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body p-5 m-5 text-white">Real Estate</h3>
+                    </div>
+                </div>                
             </div>
             <div class="card-group p-0 mr-0">
-                <div class="card">
-                    <img src="img/activities_4.png" class="card-img-top" alt="...">
-                </div>
-                <div class="card">
-                    <img src="img/activities_5.png" class="card-img-top" alt="...">
-                </div>
-                <div class="card">
-                    <img src="img/activities_6.png" class="card-img-top" alt="...">
-                </div>
+                <div class="card text-center">
+                    <img src="img/activities_4.png" class="card-img" alt="Foods and Grains">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body p-5 m-5 text-white">Foods and Grains</h3>
+                    </div>
+                </div> 
+                <div class="card text-center">
+                    <img src="img/activities_5.png" class="card-img" alt="Construction">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body p-5 m-5 text-white">Construction</h3>
+                    </div>
+                </div> 
+                <div class="card text-center">
+                    <img src="img/activities_6.png" class="card-img" alt="Machineries">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body p-5 m-5 text-white">Machineries</h3>
+                    </div>
+                </div> 
             </div>
             <div class="card-group mt-0">
-                <div class="card">
-                    <img src="img/activities_7.png" class="card-img-top" alt="...">
-                </div>
-                <div class="card">
-                    <img src="img/activities_8.png" class="card-img-top" alt="...">
-                </div>
-                <div class="card">
-                    <img src="img/activities_9.png" class="card-img-top" alt="...">
-                </div>
+                <div class="card text-center">
+                    <img src="img/activities_7.png" class="card-img" alt="IT services">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body p-5 m-5 text-white">IT services</h3>
+                    </div>
+                </div> 
+                <div class="card text-center">
+                    <img src="img/activities_8.png" class="card-img" alt="Electronics">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body p-5 m-5 text-white">Electronics</h3>
+                    </div>
+                </div> 
+                <div class="card text-center">
+                    <img src="img/activities_9.png" class="card-img" alt="Fashion Hub">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body p-5 m-5 text-white">Fashion Hub</h3>
+                    </div>
+                </div> 
             </div>
         </div>
         <!-- Forsewa Key Activities Section Closed -->
@@ -209,7 +235,8 @@
                   <h2 class="text-white">Key Activities</h2>
                   <ul style="list-style-type:none;">
                       <li><a class="footer_link" href="#">Import and Export</a></li>
-                      <li><a class="footer_link" href="package.php">Real Estate</a></li>
+                      <li><a class="footer_link" href="#">Hardware</a></li>
+                      <li><a class="footer_link" href="#">Real Estate</a></li>
                       <li><a class="footer_link" href="#">Foods and Grains</a></li>
                       <li><a class="footer_link" href="#">Construction</a></li>
                       <li><a class="footer_link" href="#">Machineries</a></li>
