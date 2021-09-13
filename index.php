68ec628e8211c0414b79097bdeedf225e2cb55c4
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-13 17:52:32 +0545

    Changes made in Homepage from office

--- index.php
+++ index.php
@@ -21,31 +21,29 @@
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3" id="bg_nav">
         <div class="container">
             <a class="navbar-brand" href="index.php">
-                <img src="img/logo.png" class="img-fluid" alt="Logo of ForSewa">
+                <img src="img/logo.png" class="img-fluid float-start" alt="Logo of ForSewa">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="float-end">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="about.php">About us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="activities.php">Activities</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="investor.php">Investor</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="contact.php">Contact Us</a>
-                        </li>
-                    </ul>
-                </div>
+            <div class="collapse navbar-collapse d-flex float-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item p-3">
+                        <a class="nav-link active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item p-3">
+                        <a class="nav-link text-white" href="about.php">About us</a>
+                    </li>
+                    <li class="nav-item p-3">
+                        <a class="nav-link text-white" href="activities.php">Activities</a>
+                    </li>
+                    <li class="nav-item p-3">
+                        <a class="nav-link text-white" href="investor.php">Investor</a>
+                    </li>
+                    <li class="nav-item p-3">
+                        <a class="nav-link text-white" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
