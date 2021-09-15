ddd2ac269b7e63d32b1130aa681bae5945becca9
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-15 11:54:20 +0545

    Changes made in Test page to check

---
+++ test.php
@@ -0,0 +1,303 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+
+<body>
+    <!-- Header Opened -->
+    <!-- NavBar started -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3" id="bg_nav">
+        <div class="container">
+            <a class="navbar-brand d-flex" href="index.php">
+                <img src="img/logo.png" class="img-fluid" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse float-end bg-primary" id="navbarNav">
+                <div class="mx-auto"></div>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item p-3">
+                        <a class="nav-link active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item p-3">
+                        <a class="nav-link text-white" href="about.php">About us</a>
+                    </li>
+                    <li class="nav-item p-3">
+                        <a class="nav-link text-white" href="activities.php">Activities</a>
+                    </li>
+                    <li class="nav-item p-3">
+                        <a class="nav-link text-white" href="investor.php">Investor</a>
+                    </li>
+                    <li class="nav-item p-3">
+                        <a class="nav-link text-white" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Banner Image  -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+          <div class="carousel-item active">
+              <img src="img/Slider_1.png" class="d-block w-100" alt="Home page Image">
+              <div class="carousel-caption d-none d-md-block">
+                  <h2 class="header_1 pb-5"> Your home <br> away from Home.</h2>
+                </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img/Slider_2.png" class="d-block w-100" alt="Home page Image">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2 class="header_1 pb-5"> Your home <br> away from Home.</h2>
+                </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img/Slider_3.png" class="d-block w-100" alt="Home page Image">  
+            <div class="carousel-caption d-none d-md-block">
+                <h2 class="header_1 pb-5"> Your home <br> away from Home.</h2>
+             </div>
+          </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <!-- Header Closed -->
+
+    <!-- Section Opened -->
+    <section>
+         <!-- Forsewa Key Activities Section Opened -->
+         <div class="container mt-5">
+            <h1 class="mb-5">ForSewa Key Activities</h1>
+            <div class="card-group activity_image">
+                <div class="card text-center">
+                    <img src="img/activities_1.png" class="card-img" alt="Import and Export">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body img-fluid text-white">Import and Export</h3>
+                    </div>
+                </div>
+                <div class="card text-center">
+                    <img src="img/activities_2.png" class="card-img" alt="Hardware">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body text-white">Hardware</h3>
+                    </div>
+                </div>
+                <div class="card text-center">
+                    <img src="img/activities_3.png" class="card-img" alt="Real Estate">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body img-fluid text-white">Real Estate</h3>
+                    </div>
+                </div>                
+            </div>
+            <div class="card-group p-0 mr-0">
+                <div class="card text-center">
+                    <img src="img/activities_4.png" class="card-img" alt="Foods and Grains">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body img-fluid text-white">Foods and Grains</h3>
+                    </div>
+                </div> 
+                <div class="card text-center">
+                    <img src="img/activities_5.png" class="card-img" alt="Construction">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body img-fluid text-white">Construction</h3>
+                    </div>
+                </div> 
+                <div class="card text-center">
+                    <img src="img/activities_6.png" class="card-img" alt="Machineries">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body img-fluid text-white">Machineries</h3>
+                    </div>
+                </div> 
+            </div>
+            <div class="card-group mt-0">
+                <div class="card text-center">
+                    <img src="img/activities_7.png" class="card-img" alt="IT services">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body img-fluid text-white">IT services</h3>
+                    </div>
+                </div> 
+                <div class="card text-center">
+                    <img src="img/activities_8.png" class="card-img" alt="Electronics">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body img-fluid text-white">Electronics</h3>
+                    </div>
+                </div> 
+                <div class="card text-center">
+                    <img src="img/activities_9.png" class="card-img" alt="Fashion Hub">
+                    <div class="card-img-overlay">
+                        <h3 class="card-body img-fluid text-white">Fashion Hub</h3>
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <!-- Forsewa Key Activities Section Closed -->
+
+        <!-- Image Session Opened -->
+        <div class="bg_image mt-5 pt-5">
+            <div class="container">
+                <h2 class="header_2 text-white">Technical Support is Essential</h2>
+            </div>
+        </div>
+        <!-- Image session Closed -->
+
+    <!-- Recent activities Session Opened -->
+       <div class="container mt-5">
+            <h2 class="mb-5">Recent Activities</h2>
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col-md-6">
+                    <div class="card bg-dark text-white">
+                        <img src="img/Recent_news_1.png" class="card-img" alt="Recent News">
+                        <div class="card-img-overlay recent_activites">
+                            <div class="card_bottom pt-5 mt-5">
+                                <p class="pt-5 mt-5">
+                                    Programiz: An e-Learning Platform from Nepal has now Reached 15 Million pageviews/month in the Global 
+                                    Market.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card bg-dark text-white">
+                        <img src="img/Recent_news_2.png" class="card-img" alt="Recent News">
+                        <div class="card-img-overlay recent_activites">
+                            <div class="card_bottom pt-5 mt-5">
+                                <p class="pt-5 mt-5">Facebook Messanger calls and Instagram DM  gets end to end encryption</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card bg-dark text-white">
+                        <img src="img/Recent_news_3.png" class="card-img-top" alt="Recent News">
+                        <div class="card-img-overlay recent_activites">
+                            <div class="card_bottom pt-5 mt-5">
+                                <p class="pt-5 mt-5">Forsewa becomes one of the best online selling portal.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card bg-dark text-white">
+                        <img src="img/Recent_news_1.png" class="card-img-top" alt="Recent News">
+                        <div class="card-img-overlay recent_activites">
+                            <div class="card_bottom pt-5 mt-5">
+                                <p class="pt-5 mt-5"> Programiz: An e-Learning Platform from Nepal has now Reached 15 Million pageviews/month in the Global 
+                                    Market</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- Recent activities Session Closed -->
+    </section>
+    <!-- Footer Session Opened -->
+    <footer class="footer-section mt-5">
+      <div class="container">
+          <div class="row d-flex justify-content-center">
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                  <h2 class="text-white">About us</h2>
+                  <ul style="list-style-type:none;">
+                      <li><a class="footer_link" href="#">About ForSewa</a></li>
+                      <li><a class="footer_link" href="package.php">Financial Reports</a></li>
+                  </ul>
+                  <div class="footer-social-icon mt-2">
+                      <a href="https://www.facebook.com/Naava-Resort-Retreat-100547551927011/"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                      <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                      <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                  </div>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                  <h2 class="text-white">Key Activities</h2>
+                  <ul style="list-style-type:none;">
+                    <li><a class="footer_link" href="import_and_export.php">Import and Export</a></li>
+                    <li><a class="footer_link" href="hardware.php">Hardware</a></li>
+                    <li><a class="footer_link" href="real_estate.php">Real Estate</a></li>
+                    <li><a class="footer_link" href="food_and_grain.php">Foods and Grains</a></li>
+                    <li><a class="footer_link" href="#">Construction</a></li>
+                    <li><a class="footer_link" href="machineries.php">Machineries</a></li>
+                    <li><a class="footer_link" href="#">IT services</a></li>
+                    <li><a class="footer_link" href="#">Electronics</a></li>
+                    <li><a class="footer_link" href="fashion_hub.php">Fashion Hub</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                <h2 class="text-white">Investor</h2>
+                  <ul style="list-style-type: none;">
+                      <li><a class="footer_link" href="#">Press Release</a></li>
+                      <li><a class="footer_link" href="about.php">Company Announcement</a></li>
+                      <li><a class="footer_link" href="contact.php">Notice to shareholder</a></li>
+                      <li><a class="footer_link" href="#">Meeting</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                  <h2 class="text-white">Contact</h2>
+                  <div class="footer-social-icon">
+                      <img src="img/logo.png" alt="Logo">
+                      <span class="text-white">Copyright © 2021 ForSewa Pvt. Ltd </span>
+                      <span class="text-white">All rights reserved  Powered by ForSewa</span>
+                      <span class="text-white">Privacy Policy</span>
+                      <span class="text-white">Code of Conduct & Business Ethics</span>
+                      <span class="text-white">Terms, Conditions and Disclaimers</span>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </footer>
+    <!-- Footer Session Closed -->
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        -->
+
+    <!--  jQuery 1.7+  -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="jquery-1.9.1.min.js"></script>
+
+
+    <!-- include scripts -->
+    <script>
+    $(document).ready(function() {
+      var nav = document.querySelector("nav");
+
+      window.addEventListener("scroll", function() {
+          if (window.pageYOffset > 100) {
+              nav.classList.add("bg-dark", "shadow");
+          } else {
+              nav.classList.remove("bg-dark", "shadow");
+          }
+      });
+
+      $(".navbar-toggler").click(function() {
+          $("#bg_nav").toggleClass("bg-menu");
+      });
+    })
+  </script>
+</body>
+
+</html>
