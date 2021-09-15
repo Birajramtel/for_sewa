2449824ced8cba85aec0e3e203f60e71986d31bf
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-15 17:24:27 +0545

    Changes made in Homepage from office

--- index.php
+++ index.php
@@ -15,30 +15,30 @@
 <body>
     <!-- Header Opened -->
     <!-- NavBar started -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3" id="bg_nav">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
         <div class="container">
-            <a class="navbar-brand" href="index.php">
+            <a class="navbar-brand p-0 m-0" href="index.php">
                 <img src="img/logo.png" class="img-fluid" alt="Logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="bg_nav collapse navbar-collapse" id="navbarNav">
                 <div class="mx-auto"></div>
                 <ul class="navbar-nav">
-                    <li class="nav-item p-3">
+                    <li class="nav-item p-3 pb-0 pt-0">
                         <a class="nav-link active" href="index.php">Home</a>
                     </li>
-                    <li class="nav-item p-3">
+                    <li class="nav-item p-3 pb-0 pt-0">
                         <a class="nav-link text-white" href="about.php">About us</a>
                     </li>
-                    <li class="nav-item p-3">
+                    <li class="nav-item p-3 pb-0 pt-0">
                         <a class="nav-link text-white" href="activities.php">Activities</a>
                     </li>
-                    <li class="nav-item p-3">
+                    <li class="nav-item p-3 pb-0 pt-0">
                         <a class="nav-link text-white" href="investor.php">Investor</a>
                     </li>
-                    <li class="nav-item p-3">
+                    <li class="nav-item p-3 pb-0 pt-0">
                         <a class="nav-link text-white" href="contact.php">Contact Us</a>
                     </li>
                 </ul>
@@ -47,104 +47,104 @@
     </nav>
     <!-- Banner Image  -->
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-          <div class="carousel-item active">
-              <img src="img/Slider_1.png" class="d-block w-100" alt="Home page Image">
-              <div class="carousel-caption d-none d-md-block">
-                  <h2 class="header_1 pb-5"> Your home <br> away from Home.</h2>
-                </div>
-          </div>
-          <div class="carousel-item">
-            <img src="img/Slider_2.png" class="d-block w-100" alt="Home page Image">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/Slider_1.png" class="d-block w-100 h-100" alt="Hotel View">
                 <div class="carousel-caption d-none d-md-block">
-                  <h2 class="header_1 pb-5"> Your home <br> away from Home.</h2>
+                    <h2 class="header_1 pb-5">Fulfilling Your Needs</h2>
                 </div>
-          </div>
-          <div class="carousel-item">
-            <img src="img/Slider_3.png" class="d-block w-100" alt="Home page Image">  
-            <div class="carousel-caption d-none d-md-block">
-                <h2 class="header_1 pb-5"> Your home <br> away from Home.</h2>
-             </div>
-          </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+            </div>
+            <div class="carousel-item">
+                <img src="img/Slider_2.png" class="d-block w-100" alt="Hotel View">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2 class="header_1 pb-5">Fulfilling Your Needs</h2>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/Slider_3.png" class="d-block w-100" alt="Hotel View">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2 class="header_1 pb-5">Fulfilling Your Needs</h2>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
     <!-- Header Closed -->
 
     <!-- Section Opened -->
     <section>
-         <!-- Forsewa Key Activities Section Opened -->
-         <div class="container mt-5">
+        <!-- Forsewa Key Activities Section Opened -->
+        <div class="container mt-5">
             <h1 class="mb-5">ForSewa Key Activities</h1>
-            <div class="card-group activity_image">
-                <div class="card text-center">
+            <div class="card-group">
+                <div class="card  text-center">
                     <img src="img/activities_1.png" class="card-img" alt="Import and Export">
-                    <div class="card-img-overlay">
-                        <a href="import_and_export.php"><h3 class="card-body img-fluid text-white">Import and Export</h3></a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="import_and_export.php" class="card-body img-fluid text-white">Import and Export</a>
                     </div>
                 </div>
                 <div class="card text-center">
                     <img src="img/activities_2.png" class="card-img" alt="Hardware">
-                    <div class="card-img-overlay">
-                        <a href="hardware.php"><h3 class="card-body text-white">Hardware</h3></a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="hardware.php" class="card-body text-white">Hardware</a>
                     </div>
                 </div>
                 <div class="card text-center">
                     <img src="img/activities_3.png" class="card-img" alt="Real Estate">
-                    <div class="card-img-overlay">
-                        <a href="real_estate.php"><h3 class="card-body img-fluid text-white">Real Estate</h3></a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="real_estate.php" class="card-body img-fluid text-white">Real Estate</a>
                     </div>
                 </div>                
             </div>
             <div class="card-group p-0 mr-0">
                 <div class="card text-center">
                     <img src="img/activities_4.png" class="card-img" alt="Foods and Grains">
-                    <div class="card-img-overlay">
-                        <a href="food_and_grain.php"><h3 class="card-body img-fluid text-white">Foods and Grains</h3></a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="food_and_grain.php" class="card-body img-fluid text-white">Foods and Grains</a>
                     </div>
                 </div> 
                 <div class="card text-center">
                     <img src="img/activities_5.png" class="card-img" alt="Construction">
-                    <div class="card-img-overlay">
-                        <a href="construction.php"><h3 class="card-body img-fluid text-white">Construction</h3></a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="construction.php" class="card-body img-fluid text-white">Construction</a>
                     </div>
                 </div> 
                 <div class="card text-center">
                     <img src="img/activities_6.png" class="card-img" alt="Machineries">
-                    <div class="card-img-overlay">
-                        <a href="machineries.php"><h3 class="card-body img-fluid text-white">Machineries</h3></a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="machineries.php" class="card-body img-fluid text-white">Machineries</a>
                     </div>
                 </div> 
             </div>
             <div class="card-group mt-0">
                 <div class="card text-center">
                     <img src="img/activities_7.png" class="card-img" alt="IT services">
-                    <div class="card-img-overlay">
-                        <a href="it_services.php"><h3 class="card-body img-fluid text-white">IT services</h3></a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="it_services.php" class="card-body img-fluid text-white">IT services</a>
                     </div>
                 </div> 
                 <div class="card text-center">
                     <img src="img/activities_8.png" class="card-img" alt="Electronics">
-                    <div class="card-img-overlay">
-                        <a href="electronics.php"><h3 class="card-body img-fluid text-white">Electronics</h3></a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="electronics.php" class="card-body img-fluid text-white">Electronics</a>
                     </div>
                 </div> 
                 <div class="card text-center">
                     <img src="img/activities_9.png" class="card-img" alt="Fashion Hub">
-                    <div class="card-img-overlay">
-                        <a href="fashion_hub.php"><h3 class="card-body img-fluid text-white">Fashion Hub</h3></a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="fashion_hub.php" class="card-body img-fluid text-white">Fashion Hub</a>
                     </div>
                 </div> 
             </div>
@@ -160,55 +160,37 @@
         <!-- Image session Closed -->
 
     <!-- Recent activities Session Opened -->
-       <div class="container mt-5">
-            <h2 class="mb-5">Recent Activities</h2>
-            <div class="row row-cols-1 row-cols-md-2 g-4">
-                <div class="col-md-6">
-                    <div class="card bg-dark text-white">
-                        <img src="img/Recent_news_1.png" class="card-img" alt="Recent News">
-                        <div class="card-img-overlay recent_activites">
-                            <div class="card_bottom pt-5 mt-5">
-                                <p class="pt-5 mt-5">
-                                    Programiz: An e-Learning Platform from Nepal has now Reached 15 Million pageviews/month in the Global 
-                                    Market.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card bg-dark text-white">
-                        <img src="img/Recent_news_2.png" class="card-img" alt="Recent News">
-                        <div class="card-img-overlay recent_activites">
-                            <div class="card_bottom pt-5 mt-5">
-                                <p class="pt-5 mt-5">Facebook Messanger calls and Instagram DM  gets end to end encryption</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card bg-dark text-white">
-                        <img src="img/Recent_news_3.png" class="card-img-top" alt="Recent News">
-                        <div class="card-img-overlay recent_activites">
-                            <div class="card_bottom pt-5 mt-5">
-                                <p class="pt-5 mt-5">Forsewa becomes one of the best online selling portal.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card bg-dark text-white">
-                        <img src="img/Recent_news_1.png" class="card-img-top" alt="Recent News">
-                        <div class="card-img-overlay recent_activites">
-                            <div class="card_bottom pt-5 mt-5">
-                                <p class="pt-5 mt-5"> Programiz: An e-Learning Platform from Nepal has now Reached 15 Million pageviews/month in the Global 
-                                    Market</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="container mt-5">
+        <h1 class="pb-3">About Us</h1>
+                <p class="para_align">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et enim sapien. Curabitur tortor elit, 
+                    feugiat at dui nec, vestibulum placerat turpis. Donec tempor ipsum sit amet tempor convallis. Morbi 
+                    iaculis, nibh sed congue ultrices, ipsum nibh dignissim turpis, id facilisis lacus risus a sapien. 
+                    Nulla in sodales mauris. Fusce tempor posuere est, id bibendum urna sodales in. Aliquam ac orci ullamcorper, 
+                    volutpat orci non, euismod turpis. Nam a iaculis felis. Etiam risus felis, eleifend non gravida sed, viverra 
+                    non lacus. Quisque tristique ipsum auctor laoreet consequat. Fusce varius cursus convallis. Vivamus gravida 
+                    erat quis velit fringilla molestie.
+                </p>
+                <br>
+                <p class="para_align">
+                    Nam sed risus quis velit mattis tempor. Nunc non libero blandit lacus scelerisque luctus rhoncus at nunc. 
+                    Vivamus non tempus lorem. Vestibulum at maximus sem. Donec et est ac arcu tincidunt mollis ut sed neque. 
+                    In sollicitudin efficitur felis, in auctor sapien lobortis sit amet. Integer suscipit porta nulla quis 
+                    pretium. Vivamus non velit eu leo consequat laoreet. Donec eget augue nisi. Suspendisse posuere pulvinar 
+                    iaculis. Quisque pretium pretium sem in sodales. Praesent ultrices metus vitae eros bibendum rhoncus. Mauris 
+                    at consequat purus. Nulla orci nulla, pellentesque a convallis non, tempus vitae eros. Praesent sem mauris, 
+                    consequat vel ligula ut, volutpat condimentum felis. 
+                </p>
+                <br>
+                <p class="para_align">
+                    Nam sed risus quis velit mattis tempor. Nunc non libero blandit lacus scelerisque luctus rhoncus at nunc. 
+                    Vivamus non tempus lorem. Vestibulum at maximus sem. Donec et est ac arcu tincidunt mollis ut sed neque. 
+                    In sollicitudin efficitur felis, in auctor sapien lobortis sit amet. Integer suscipit porta nulla quis 
+                    pretium. Vivamus non velit eu leo consequat laoreet. Donec eget augue nisi. Suspendisse posuere pulvinar 
+                    iaculis. Quisque pretium pretium sem in sodales. Praesent ultrices metus vitae eros bibendum rhoncus. Mauris
+                    at consequat purus. 
+                </p>
+    </div>
     <!-- Recent activities Session Closed -->
     </section>
     <!-- Footer Session Opened -->
@@ -294,7 +276,7 @@
       });
 
       $(".navbar-toggler").click(function() {
-          $("#bg_nav").toggleClass("bg-menu");
+          $(".bg_nav").toggleClass("bg-menu");
       });
     })
   </script>
