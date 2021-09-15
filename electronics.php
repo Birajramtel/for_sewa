af5c6aa0db85975c60426ffe20e4287990636e9f
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-15 10:43:28 +0545

    Changes made in Electronics page

---
+++ electronics.php
@@ -0,0 +1,253 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" integrity="sha512-UTNP5BXLIptsaj5WdKFrkFov94lDx+eBvbKyoe1YAfjeRPC+gT5kyZ10kOHCfNZqEui1sxmqvodNUx3KbuYI/A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="js/owl.carousel.js">
+    <link rel="stylesheet" href="js/owl.carousel.min.js">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Header Opened -->
+    <!-- NavBar started -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3" id="bg_nav">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="img/logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="mx-auto">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="about.php">About us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="activities.php">Activities</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="investor.php">Investor</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="contact.php">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Banner Image  -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+          <div class="carousel-item active">
+              <img src="img/Slider_1.png" class="d-block w-100" alt="Home page Image">
+              <div class="carousel-caption d-none d-md-block">
+                  <h2 class="header_1 pb-5"> Your home <br> away from Home.</h2>
+                </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img/Slider_2.png" class="d-block w-100" alt="Home page Image">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2 class="header_1 pb-5"> Your home <br> away from Home.</h2>
+                </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img/Slider_3.png" class="d-block w-100" alt="Home page Image">  
+            <div class="carousel-caption d-none d-md-block">
+                <h2 class="header_1 pb-5"> Your home <br> away from Home.</h2>
+             </div>
+          </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <!-- Header Closed -->
+
+    <!-- Section Opened -->
+    <section>
+        <!-- About Us page Opened -->
+        <h1 class="text-white pt-5">Construction</h1>
+        <!-- About Us page Closed -->
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col-md-6">
+                    <div class="card">
+                        <img src="img/electronics_1.png" class="card-img-top" alt="Electronics">
+                        <div class="card-body">
+                            <h5 class="card-title Header_2">Electronics</h5>
+                            <p class="card-text para_align">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et enim sapien. Curabitur 
+                                tortor elit, feugiat at dui nec, vestibulum plaicerat turpis. Donec tempor ipsum sit 
+                                amet tempor convallis. Morbi iaculis, nibh sed congue ultrices, ipsum nibh dignissim 
+                                turpis, id facilisis lacus risus a sapien. Nulla in sodales mauris. Fusce tempor posuere 
+                                est, id bibendum urna sodales in. Aliquam ac orci ullamcorper, volutpat orci non, euismod 
+                                turpis. Nam a iaculis felis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <img src="img/electronics_2.png" class="card-img-top" alt="Electronics">
+                        <div class="card-body">
+                            <h5 class="card-title Header_2">Electronics Items</h5>
+                            <p class="card-text para_align">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et enim sapien. Curabitur 
+                                tortor elit, feugiat at dui nec, vestibulum plaicerat turpis. Donec tempor ipsum sit 
+                                amet tempor convallis. Morbi iaculis, nibh sed congue ultrices, ipsum nibh dignissim 
+                                turpis, id facilisis lacus risus a sapien. Nulla in sodales mauris. Fusce tempor posuere 
+                                est, id bibendum urna sodales in. Aliquam ac orci ullamcorper, volutpat orci non, euismod 
+                                turpis. Nam a iaculis felis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <img src="img/electronics_3.png" class="card-img-top" alt="Electronics">
+                        <div class="card-body">
+                            <h5 class="card-title Header_2">Electronics Items</h5>
+                            <p class="card-text para_align pb-0">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et enim sapien. Curabitur 
+                                tortor elit, feugiat at dui nec, vestibulum plaicerat turpis. Donec tempor ipsum sit 
+                                amet tempor convallis. Morbi iaculis, nibh sed congue ultrices, ipsum nibh dignissim 
+                                turpis, id facilisis lacus risus a sapien. Nulla in sodales mauris. Fusce tempor posuere 
+                                est, id bibendum urna sodales in. Aliquam ac orci ullamcorper, volutpat orci non, euismod 
+                                turpis. Nam a iaculis felis.
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <img src="img/electronics_4.png" class="card-img-top" alt="Electronics">
+                        <div class="card-body">
+                            <h5 class="card-title Header_2">Electronics Items</h5>
+                            <p class="card-text para_align">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et enim sapien. Curabitur 
+                                tortor elit, feugiat at dui nec, vestibulum plaicerat turpis. Donec tempor ipsum sit 
+                                amet tempor convallis. Morbi iaculis, nibh sed congue ultrices, ipsum nibh dignissim 
+                                turpis, id facilisis lacus risus a sapien. Nulla in sodales mauris. Fusce tempor posuere 
+                                est, id bibendum urna sodales in. Aliquam ac orci ullamcorper, volutpat orci non, euismod 
+                                turpis. Nam a iaculis felis. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer Session Opened -->
+    <footer class="footer-section mt-5">
+      <div class="container">
+          <div class="row d-flex justify-content-center">
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                  <h2 class="text-white">About us</h2>
+                  <ul style="list-style-type:none;">
+                      <li><a class="footer_link" href="#">About ForSewa</a></li>
+                      <li><a class="footer_link" href="package.php">Financial Reports</a></li>
+                  </ul>
+                  <div class="footer-social-icon mt-2">
+                      <a href="https://www.facebook.com/Naava-Resort-Retreat-100547551927011/"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                      <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                      <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                  </div>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                  <h2 class="text-white">Key Activities</h2>
+                  <ul style="list-style-type:none;">
+                    <li><a class="footer_link" href="import_and_export.php">Import and Export</a></li>
+                    <li><a class="footer_link" href="hardware.php">Hardware</a></li>
+                    <li><a class="footer_link" href="real_estate.php">Real Estate</a></li>
+                    <li><a class="footer_link" href="food_and_grain.php">Foods and Grains</a></li>
+                    <li><a class="footer_link" href="#">Construction</a></li>
+                    <li><a class="footer_link" href="machineries.php">Machineries</a></li>
+                    <li><a class="footer_link" href="#">IT services</a></li>
+                    <li><a class="footer_link" href="#">Electronics</a></li>
+                    <li><a class="footer_link" href="fashion_hub.php">Fashion Hub</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                <h2 class="text-white">Investor</h2>
+                  <ul style="list-style-type: none;">
+                      <li><a class="footer_link" href="#">Press Release</a></li>
+                      <li><a class="footer_link" href="about.php">Company Announcement</a></li>
+                      <li><a class="footer_link" href="contact.php">Notice to shareholder</a></li>
+                      <li><a class="footer_link" href="#">Meeting</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                  <h2 class="text-white">Contact</h2>
+                  <div class="footer-social-icon">
+                      <img src="img/logo.png" alt="Logo">
+                      <span class="text-white">Copyright © 2021 ForSewa Pvt. Ltd </span>
+                      <span class="text-white">All rights reserved  Powered by ForSewa</span>
+                      <span class="text-white">Privacy Policy</span>
+                      <span class="text-white">Code of Conduct & Business Ethics</span>
+                      <span class="text-white">Terms, Conditions and Disclaimers</span>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </footer>
+    <!-- Footer Session Closed -->
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+
+    <!--  jQuery 1.7+  -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="jquery-1.9.1.min.js"></script>
+
+
+    <!-- include scripts -->
+    <script>
+    $(document).ready(function() {
+      var nav = document.querySelector("nav");
+
+      window.addEventListener("scroll", function() {
+          if (window.pageYOffset > 100) {
+              nav.classList.add("bg-dark", "shadow");
+          } else {
+              nav.classList.remove("bg-dark", "shadow");
+          }
+      });
+
+      $(".navbar-toggler").click(function() {
+          $("#bg_nav").toggleClass("bg-menu");
+      });
+    })
+  </script>
+</body>
+
+</html>
