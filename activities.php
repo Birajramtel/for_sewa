2cbbf2a74c91a56d8f47834675c11a2643fb7b2c
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-16 12:04:33 +0545

    Changes made in Activities page

--- activities.php
+++ activities.php
@@ -15,63 +15,63 @@
         <!-- Forsewa Key Activities Section Opened -->
         <div class="container mt-5">
             <h1 class="mb-5">ForSewa Key Activities</h1>
-            <div class="card-group activity_image">
-                <div class="card text-center">
+            <div class="card-group">
+                <div class="card  text-center">
                     <img src="img/activities_1.png" class="card-img" alt="Import and Export">
-                    <div class="card-img-overlay">
-                        <a href="import_and_export.php" class="activity_hero card-body img-fluid text-white">Import and Export</a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="import_and_export.php" class="card-body img-fluid text-white">Import and Export</a>
                     </div>
                 </div>
                 <div class="card text-center">
                     <img src="img/activities_2.png" class="card-img" alt="Hardware">
-                    <div class="card-img-overlay">
-                        <a href="hardware.php" class="activity_hero card-body text-white">Hardware</a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="hardware.php" class="card-body text-white">Hardware</a>
                     </div>
                 </div>
                 <div class="card text-center">
                     <img src="img/activities_3.png" class="card-img" alt="Real Estate">
-                    <div class="card-img-overlay">
-                        <a href="real_estate.php" class="activity_hero card-body img-fluid text-white">Real Estate</a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="real_estate.php" class="card-body img-fluid text-white">Real Estate</a>
                     </div>
                 </div>                
             </div>
             <div class="card-group p-0 mr-0">
                 <div class="card text-center">
                     <img src="img/activities_4.png" class="card-img" alt="Foods and Grains">
-                    <div class="card-img-overlay">
-                        <a href="food_and_grain.php" class="activity_hero card-body img-fluid text-white">Foods and Grains</a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="food_and_grain.php" class="card-body img-fluid text-white">Foods and Grains</a>
                     </div>
                 </div> 
                 <div class="card text-center">
                     <img src="img/activities_5.png" class="card-img" alt="Construction">
-                    <div class="card-img-overlay">
-                        <a href="construction.php" class="activity_hero card-body img-fluid text-white">Construction</a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="construction.php" class="card-body img-fluid text-white">Construction</a>
                     </div>
                 </div> 
                 <div class="card text-center">
                     <img src="img/activities_6.png" class="card-img" alt="Machineries">
-                    <div class="card-img-overlay">
-                        <a href="machineries.php" class="activity_hero card-body img-fluid text-white">Machineries</a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="machineries.php" class="card-body img-fluid text-white">Machineries</a>
                     </div>
                 </div> 
             </div>
             <div class="card-group mt-0">
                 <div class="card text-center">
                     <img src="img/activities_7.png" class="card-img" alt="IT services">
-                    <div class="card-img-overlay">
-                        <a href="it_services.php" class="activity_hero card-body img-fluid text-white">IT services</a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="it_services.php" class="card-body img-fluid text-white">IT services</a>
                     </div>
                 </div> 
                 <div class="card text-center">
                     <img src="img/activities_8.png" class="card-img" alt="Electronics">
-                    <div class="card-img-overlay">
-                        <a href="electronics.php" class="activity_hero card-body img-fluid text-white">Electronics</a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="electronics.php" class="card-body img-fluid text-white">Electronics</a>
                     </div>
                 </div> 
                 <div class="card text-center">
                     <img src="img/activities_9.png" class="card-img" alt="Fashion Hub">
-                    <div class="card-img-overlay">
-                        <a href="fashion_hub.php" class="activity_hero card-body img-fluid text-white">Fashion Hub</a>
+                    <div class="card-img-overlay d-flex justify-content-center align-items-center">
+                        <a href="fashion_hub.php" class="card-body img-fluid text-white">Fashion Hub</a>
                     </div>
                 </div> 
             </div>
