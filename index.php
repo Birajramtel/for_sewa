7a2e331debab345c376abdc7a8f0a0c2abe45a12
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-16 11:17:52 +0545

    Changes made in Home page

--- index.php
+++ index.php
@@ -2,86 +2,14 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    
+    <?php include('./include/head.php') ?>    
 </head>
 
 <body>
     <!-- Header Opened -->
-    <!-- NavBar started -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
-        <div class="container">
-            <a class="navbar-brand p-0 m-0" href="index.php">
-                <img src="img/logo.png" class="img-fluid" alt="Logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="bg_nav collapse navbar-collapse" id="navbarNav">
-                <div class="mx-auto"></div>
-                <ul class="navbar-nav">
-                    <li class="nav-item p-3 pb-0 pt-0">
-                        <a class="nav-link active" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item p-3 pb-0 pt-0">
-                        <a class="nav-link text-white" href="about.php">About us</a>
-                    </li>
-                    <li class="nav-item p-3 pb-0 pt-0">
-                        <a class="nav-link text-white" href="activities.php">Activities</a>
-                    </li>
-                    <li class="nav-item p-3 pb-0 pt-0">
-                        <a class="nav-link text-white" href="investor.php">Investor</a>
-                    </li>
-                    <li class="nav-item p-3 pb-0 pt-0">
-                        <a class="nav-link text-white" href="contact.php">Contact Us</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- Banner Image  -->
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="img/Slider_1.png" class="d-block w-100" alt="Hotel View">
-                <div class="carousel-caption d-none d-md-block">
-                    <h2 class="header_1 pb-5">Fulfilling Your Needs</h2>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="img/Slider_2.png" class="d-block w-100" alt="Hotel View">
-                <div class="carousel-caption d-none d-md-block">
-                    <h2 class="header_1 pb-5">Fulfilling Your Needs</h2>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="img/Slider_3.png" class="d-block w-100" alt="Hotel View">
-                <div class="carousel-caption d-none d-md-block">
-                    <h2 class="header_1 pb-5">Fulfilling Your Needs</h2>
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-    <!-- Header Closed -->
+    <header>
+        <?php include('./include/header.php') ?>
+    </header>  
 
     <!-- Section Opened -->
     <section>
@@ -194,58 +122,7 @@
     <!-- Recent activities Session Closed -->
     </section>
     <!-- Footer Session Opened -->
-    <footer class="footer-section mt-5">
-      <div class="container">
-          <div class="row d-flex justify-content-center">
-              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
-                  <h2 class="text-white">About us</h2>
-                  <ul style="list-style-type:none;">
-                      <li><a class="footer_link" href="about.php">About ForSewa</a></li>
-                      <li><a class="footer_link" href="package.php">Financial Reports</a></li>
-                  </ul>
-                  <div class="footer-social-icon mt-2">
-                      <a href="https://www.facebook.com/Naava-Resort-Retreat-100547551927011/"><i class="fab fa-facebook-f facebook-bg"></i></a>
-                      <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
-                      <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
-                  </div>
-              </div>
-              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
-                  <h2 class="text-white">Key Activities</h2>
-                  <ul style="list-style-type:none;">
-                    <li><a class="footer_link" href="import_and_export.php">Import and Export</a></li>
-                    <li><a class="footer_link" href="hardware.php">Hardware</a></li>
-                    <li><a class="footer_link" href="real_estate.php">Real Estate</a></li>
-                    <li><a class="footer_link" href="food_and_grain.php">Foods and Grains</a></li>
-                    <li><a class="footer_link" href="#">Construction</a></li>
-                    <li><a class="footer_link" href="machineries.php">Machineries</a></li>
-                    <li><a class="footer_link" href="#">IT services</a></li>
-                    <li><a class="footer_link" href="#">Electronics</a></li>
-                    <li><a class="footer_link" href="fashion_hub.php">Fashion Hub</a></li>
-                  </ul>
-              </div>
-              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
-                <h2 class="text-white">Investor</h2>
-                  <ul style="list-style-type: none;">
-                      <li><a class="footer_link" href="#">Press Release</a></li>
-                      <li><a class="footer_link" href="about.php">Company Announcement</a></li>
-                      <li><a class="footer_link" href="contact.php">Notice to shareholder</a></li>
-                      <li><a class="footer_link" href="#">Meeting</a></li>
-                  </ul>
-              </div>
-              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
-                  <h2 class="text-white">Contact</h2>
-                  <div class="footer-social-icon">
-                      <img src="img/logo.png" alt="Logo">
-                      <span class="text-white">Copyright © 2021 ForSewa Pvt. Ltd </span>
-                      <span class="text-white">All rights reserved  Powered by ForSewa</span>
-                      <span class="text-white">Privacy Policy</span>
-                      <span class="text-white">Code of Conduct & Business Ethics</span>
-                      <span class="text-white">Terms, Conditions and Disclaimers</span>
-                  </div>
-              </div>
-          </div>
-        </div>
-    </footer>
+    <?php include('./include/footer.php') ?>
     <!-- Footer Session Closed -->
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -263,23 +140,7 @@
 
 
     <!-- include scripts -->
-    <script>
-    $(document).ready(function() {
-      var nav = document.querySelector("nav");
-
-      window.addEventListener("scroll", function() {
-          if (window.pageYOffset > 100) {
-              nav.classList.add("bg-dark", "shadow");
-          } else {
-              nav.classList.remove("bg-dark", "shadow");
-          }
-      });
-
-      $(".navbar-toggler").click(function() {
-          $(".bg_nav").toggleClass("bg-menu");
-      });
-    })
-  </script>
+    <?php include('./include/scripts.php') ?>
 </body>
 
 </html>
