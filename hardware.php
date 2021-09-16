e710c51adea5fe344852c03b021cb808fd3e9db2
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-16 15:48:52 +0545

    Changes made in Hardware page

--- hardware.php
+++ hardware.php
@@ -13,15 +13,15 @@
     <!-- Section Opened -->
     <section>
         <!-- About Us page Opened -->
-        <h1 class="text-white pt-5">Hardware</h1>
         <!-- About Us page Closed -->
         <div class="container">
+            <h2 class="pt-5 pb-2 "><strong>Hardware</strong></h2>
             <div class="row row-cols-1 row-cols-md-2 g-4">
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <div class="card">
                         <img src="img/Hardware_1.png" class="card-img-top" alt="DSLR Camera">
                         <div class="card-body">
-                            <h5 class="card-title Header_2"><strong>DSLR Camera</strong></h5>
+                            <h5 class="card-title Header_2 text-center"><strong>DSLR Camera</strong></h5>
                             <p class="card-text para_align">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et enim sapien. Curabitur 
                                 tortor elit, feugiat at dui nec, vestibulum plaicerat turpis. Donec tempor ipsum sit 
@@ -33,11 +33,11 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <div class="card">
                         <img src="img/Hardware_2.png" class="card-img-top" alt="Memory">
                         <div class="card-body">
-                            <h5 class="card-title Header_2"><strong>RAM Memory</strong></h5>
+                            <h5 class="card-title Header_2 text-center"><strong>RAM Memory</strong></h5>
                             <p class="card-text para_align">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et enim sapien. Curabitur 
                                 tortor elit, feugiat at dui nec, vestibulum plaicerat turpis. Donec tempor ipsum sit 
@@ -49,11 +49,11 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 ">
+                <div class="col-md-4">
                     <div class="card">
                         <img src="img/Hardware_3.png" class="card-img-top" alt="Motherboard">
                         <div class="card-body">
-                            <h5 class="card-title Header_2"><strong>Motherboard</strong></h5>
+                            <h5 class="card-title Header_2 text-center"><strong>Motherboard</strong></h5>
                             <p class="card-text para_align pb-0">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et enim sapien. Curabitur 
                                 tortor elit, feugiat at dui nec, vestibulum plaicerat turpis. Donec tempor ipsum sit 
@@ -65,7 +65,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <!-- <div class="col-md-6">
                     <div class="card">
                         <img src="img/Hardware_4.png" class="card-img-top" alt="Intel Processor">
                         <div class="card-body">
@@ -80,7 +80,7 @@
                             </p>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </section>
