afcf0e4ecc531f1fb4209c39d2f9dce64d57ea85
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-16 11:19:21 +0545

    Changes made in Footer page

---
+++ include/footer.php
@@ -0,0 +1,52 @@
+<footer class="footer-section mt-5 mb-1">
+      <div class="container">
+          <div class="row d-flex justify-content-center">
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                  <h2 class="text-white pb-2">About us</h2>
+                  <ul style="list-style-type:none;" class="p-0">
+                      <li class="pb-3 pt-2"><a class="footer_link" href="about.php">About ForSewa</a></li>
+                      <li class="pb-1"><a class="footer_link" href="package.php">Financial Reports</a></li>
+                  </ul>
+                  <div class="footer-social-icon mt-2">
+                      <a href="https://www.facebook.com/Naava-Resort-Retreat-100547551927011/"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                      <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                      <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                  </div>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                  <h2 class="text-white">Key Activities</h2>
+                  <ul style="list-style-type:none;" class="p-0">
+                    <li class="pb-3 pt-4"><a class="footer_link" href="import_and_export.php">Import and Export</a></li>
+                    <li class="pb-3"><a class="footer_link" href="hardware.php">Hardware</a></li>
+                    <li class="pb-3"><a class="footer_link" href="real_estate.php">Real Estate</a></li>
+                    <li class="pb-3"><a class="footer_link" href="food_and_grain.php">Foods and Grains</a></li>
+                    <li class="pb-3"><a class="footer_link" href="#">Construction</a></li>
+                    <li class="pb-3"><a class="footer_link" href="machineries.php">Machineries</a></li>
+                    <li class="pb-3"><a class="footer_link" href="#">IT services</a></li>
+                    <li class="pb-3"><a class="footer_link" href="#">Electronics</a></li>
+                    <li class="pb-3"><a class="footer_link" href="fashion_hub.php">Fashion Hub</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                <h2 class="text-white">Investor</h2>
+                  <ul style="list-style-type: none;" class="p-0">
+                      <li class="pb-3 pt-4"><a class="footer_link" href="#">Press Release</a></li>
+                      <li class="pb-3"><a class="footer_link" href="about.php">Company Announcement</a></li>
+                      <li class="pb-3"><a class="footer_link" href="contact.php">Notice to shareholder</a></li>
+                      <li class="pb-3"><a class="footer_link" href="#">Meeting</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-3 col-sm-6 col-xm-12 mt-3 mb-4">
+                  <h2 class="text-white">Contact</h2>
+                  <div class="footer-social-icon pt-4">
+                      <img src="img/logo.png" class="pb-3 img-fluid" alt="Logo">
+                      <span class="text-white pb-3">Copyright © 2021 ForSewa Pvt. Ltd </span>
+                      <span class="text-white pb-3">All rights reserved  Powered by ForSewa</span>
+                      <span class="text-white pb-3">Privacy Policy</span>
+                      <span class="text-white pb-3">Code of Conduct & Business Ethics</span>
+                      <span class="text-white pb-3">Terms, Conditions and Disclaimers</span>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </footer>
