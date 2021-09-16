284d63be7c49f9a7defc68cf5383ecfd77ab7fde
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-16 11:20:29 +0545

    Header section added

---
+++ include/header.php
@@ -0,0 +1,68 @@
+ <!-- NavBar started -->
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+        <div class="container">
+            <a class="navbar-brand p-0 m-0" href="index.php">
+                <img src="img/logo.png" class="img-fluid" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="bg_nav collapse navbar-collapse" id="navbarNav">
+                <div class="mx-auto"></div>
+                <ul class="navbar-nav">
+                    <li class="nav-item p-3 pb-0 pt-0">
+                        <a class="nav-link active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item p-3 pb-0 pt-0">
+                        <a class="nav-link text-white" href="about.php">About us</a>
+                    </li>
+                    <li class="nav-item p-3 pb-0 pt-0">
+                        <a class="nav-link text-white" href="activities.php">Activities</a>
+                    </li>
+                    <li class="nav-item p-3 pb-0 pt-0">
+                        <a class="nav-link text-white" href="investor.php">Investor</a>
+                    </li>
+                    <li class="nav-item p-3 pb-0 pt-0">
+                        <a class="nav-link text-white" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Banner Image  -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/Slider_1.png" class="d-block img-fluid" alt="Import And Export">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2 class="header_1 d-md-none d-lg-block">Fulfilling Your Needs</h2>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/Slider_2.png" class="d-block img-fluid" alt="Hotel View">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2 class="header_1 d-md-none d-lg-block">Fulfilling Your Needs</h2>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/Slider_3.png" class="d-block img-fluid" alt="Hotel View">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2 class="header_1 d-md-none d-lg-block">Fulfilling Your Needs</h2>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!-- Header Closed -->
