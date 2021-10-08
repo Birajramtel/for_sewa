335e7f9aadd604dc8fbf3ee5c290f64dd65daef7
Birajramtel <biraj.ramtel007@gmail.com>
2021-10-08 12:42:20 +0545

    Typeit added to banner image

---
+++ js/typeit.js
@@ -0,0 +1,45 @@
+new TypeIt("#services", {
+    speed: 50,
+    waitUntilVisible: true,
+  })
+    
+    .type(" Import and Export")
+    .pause(600)
+    .delete(17)
+    .type(" Hardware")
+    .pause(600)
+    .delete(8)
+    .type(" Real Estate")
+    .pause(600)
+    .delete(11)
+    .go();
+new TypeIt("#services_1", {
+    speed: 50,
+    waitUntilVisible: true,
+  })
+    
+    .type(" Import and Export")
+    .pause(500)
+    .delete(17)
+    .type(" Hardware")
+    .pause(500)
+    .delete(8)
+    .type(" Real Estate")
+    .pause(500)
+    .delete(11)
+    .go();
+new TypeIt("#services_2", {
+    speed: 50,
+    waitUntilVisible: true,
+  })
+    
+    .type(" Import and Export")
+    .pause(500)
+    .delete(17)
+    .type(" Hardware")
+    .pause(500)
+    .delete(8)
+    .type(" Real Estate")
+    .pause(500)
+    .delete(11)
+    .go();
