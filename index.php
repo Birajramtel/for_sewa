fe86d9bac41844bb3e17c196c3eff85c2a885180
Birajramtel <biraj.ramtel007@gmail.com>
2021-10-18 15:12:18 +0545

    Changes made in Homepage slider part

--- index.php
+++ index.php
@@ -122,24 +122,7 @@
     </section>
     <!-- Footer Session Opened -->
     <?php include('./include/footer.php') ?>
-    <!-- Footer Session Closed -->
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-        -->
-
-    <!--  jQuery 1.7+  -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="jquery-1.9.1.min.js"></script>
-    <!-- include scripts -->
-    <?php include('./include/scripts.php') ?>
-    <script src="https://unpkg.com/typeit@8.0.3/dist/index.umd.js"></script>
-    <script src="./js/typeit.js"></script>
+    
 </body>
 
 </html>
