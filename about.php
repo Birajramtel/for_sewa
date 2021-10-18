b6f849a95a79b766a31ac80abf37f90f4a346328
Birajramtel <biraj.ramtel007@gmail.com>
2021-10-18 10:09:22 +0545

    Changes made in About us page

--- about.php
+++ about.php
@@ -8,7 +8,45 @@
 <body>
     <!-- Header Opened -->
     <header>
-        <?php include('./include/header.php') ?>
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/homepage.png" class="d-block img-fluid" alt="Import And Export">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2 class="header_1 d-md-none d-lg-block pt-5 mt-4">Chain of Trust and Support</h2>
+                        <p id="services"> Our Services - </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Import and Export.png" class="d-block img-fluid" alt="Electronics">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p></p>
+                        <h2 class="header_1 d-md-none d-lg-block pt-5 mt-4">Chain of Trust and Support</h2>
+                        <p id="services_1" > Our Services - </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Real Estate.png" class="d-block img-fluid h-100" alt="Fashion Hub">
+                    <div class="carousel-caption d-none d-md-block">
+                    <h2 class="header_1 d-md-none d-lg-block pt-5 mt-4">Chain of Trust and Support</h2>
+                        <p id="services_2"> Our Services - </p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </header>  
     <!-- Section Opened -->
     <section>
