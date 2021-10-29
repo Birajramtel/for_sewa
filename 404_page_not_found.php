15fd3df83a1549fe53f769ed832eda5151da05a2
Birajramtel <biraj.ramtel007@gmail.com>
2021-10-29 11:38:44 +0545

    404 Page not found

---
+++ 404_page_not_found.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include('./include/head.php') ?>
+</head>
+
+<body>
+    <!-- Header Opened -->
+    <header>
+         <!-- NavBar started -->
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3 nav_relative">
+            <div class="container">
+                <a class="navbar-brand p-0 m-0 " href="index.php">
+                    <img src="img/Forsewa_logo.png" class="img-fluid w-50 d-block icon_logo" alt="Logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="bg_nav collapse navbar-collapse" id="navbarNav">
+                    <div class="mx-auto"></div>
+                    <ul class="navbar-nav text_change">
+                        <li class="nav-item p-3 pb-0 pt-0">
+                            <a class="nav-link text-dark" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item p-3 pb-0 pt-0">
+                            <a class="nav-link text-dark" href="about.php">About us</a>
+                        </li>
+                        <li class="nav-item p-3 pb-0 pt-0">
+                            <a class="nav-link text-dark" href="activities.php">Activities</a>
+                        </li>
+                        <li class="nav-item p-3 pb-0 pt-0">
+                            <a class="nav-link text-dark" href="investor.php">Investor</a>
+                        </li>
+                        <li class="nav-item p-3 pb-0 pt-0">
+                            <a class="nav-link text-dark" href="contact.php">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    <!-- Banner Image  -->
+    <img src="img/404_page_not_found.png" class="img-fluid" alt="404 Page not found">
+        
+    <!-- Header Closed -->
+    </header>  
+    <!-- Header Closed -->
+    <!-- Section Opened -->
+    <section>
+        <!-- About Us page Opened -->
+        <div class="container d-flex justify-content-center">
+            <span class="header_1 pt-5 pb-5 ">404 Page not found</span> 
+
+        </div>
+    </section>
+    <!-- Footer Session Opened -->
+    <?php include('./include/footer.php') ?>
+    
+    <?php include('./include/scripts.php') ?>
+    
+</body>
+
+</html>
